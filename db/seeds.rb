ead6970ee0091eb75f267804d5ed7078b5792e8d
Oksana Hoeckele <oksanaskupa@gmail.com>
2017-09-17 14:25:40 -0400

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+["Fantasy", "Technology", "Education"].each do |cat|
+  Category.find_or_create_by(name: cat)
+end
